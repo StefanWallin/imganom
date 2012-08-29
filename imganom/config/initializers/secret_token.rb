b3590a11967447494d6e9920ecf597c75ec4f7d2
Stefan Wallin <stefan.w@festiz.com>
2012-08-29 19:05:39 +0200

    Screw rails, let's try sinatra instead

--- imganom/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Imganom::Application.config.secret_token = 'a3fcf9dbaca72dba9480243a837c6f1fa9bc32a210f7a69a6413bd638e43a2b67b62d57a30d48b0f57a63a8d2d56d1765e7dbe6fd75a95c6bffa7f163f1fbed3'
